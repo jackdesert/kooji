cc072914abb7c962507b93258ef0302fcbdde46e
Jack Desert <jack@healthtalker.com>
2011-12-25 16:16:09 -0500

    put auth logic fields into inital migration because otherwise it was messy to have a not-null field with no default value

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(:version => 20111225183326) do
     t.string   "last_name"
     t.string   "phone_cell"
     t.string   "experience"
+    t.string   "persistence_token",                  :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_type"
