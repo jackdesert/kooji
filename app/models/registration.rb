8d636218584e298c4430317f17120fcf1ca352dc
Jack Desert <jack@healthtalker.com>
2011-12-26 17:26:30 -0500

    added composite unique constraint to registration.rb

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,6 @@
 class Registration < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  # this is a composite uniqueness constraint
+  validates_uniqueness_of :user_id, :scope => :event_id
 end
