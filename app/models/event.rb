49ac4519a162c127ac933fb0c886cd99f093185d
Jack Desert <jack@healthtalker.com>
2011-12-26 09:15:42 -0500

    added foreign keys to registration model added belongs to clauses and has many through clauses

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
+  has_many :registrations
+  has_many :users, :through => :registrations
+
   validates_presence_of :event_status, :event_is_program, :event_name, :pricing, :description, :confirmation_page, :start_date
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :registrations
+  has_many :events, :through => :registrations
+
   acts_as_authentic do |config|
     # Add custom configuration options here
     config.crypto_provider = Authlogic::CryptoProviders::Sha1
