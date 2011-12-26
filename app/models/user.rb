642f970da412ecff84e53a61b2370930f42b6e9a
Jack Desert <jack@healthtalker.com>
2011-12-26 01:21:42 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ActiveRecord::Base
     config.crypto_provider = Authlogic::CryptoProviders::Sha1
   end
 
-  validates_format_of :first_name, :with => /[a-zA-Z ]/
+  validates_format_of :first_name, :with => /^[a-zA-Z ]+$/, :message => "(Names can only contain letters)"
+  validates_format_of :last_name, :with => /^[a-zA-Z ]+$/, :message => "(Names can only contain letters)"
+  validates_format_of :phone_cell, :with => /^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/, :message => "Use this phone number format: XXX-XXX-XXXX"
+  validates_format_of :phone_evening, :with => /^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/, :message => "Use this phone number format: XXX-XXX-XXXX"
+  validates_presence_of :experience, :exercise, :medical, :emergency_contact, :diet
 end
