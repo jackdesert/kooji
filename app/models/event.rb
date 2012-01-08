8649e3a201924796f14e457e2397004cbe2b176b
Jack Desert <jack@hengage.com>
2012-01-08 10:25:24 -0500

    event spec is passing for all three types of date ranges

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,9 +11,15 @@ class Event < ActiveRecord::Base
       date = self.start_date.strftime("%A %b %e, %Y")
       return date.single_space
     end
-    first = self.start_date.strftime("%A %b %e")
-    last = self.end_date.strftime("%A %b %e, %Y")
-    first.single_space + " - " + last.single_space
+    if self.start_date.year == self.end_date.year
+      first = self.start_date.strftime("%A %b %e")
+      last = self.end_date.strftime("%A %b %e, %Y")
+      return first.single_space + " - " + last.single_space
+    else
+      first = self.start_date.strftime("%A %b %e, %Y")
+      last = self.end_date.strftime("%A %b %e, %Y")
+      return first.single_space + " - " + last.single_space
+    end
   end
 
 
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -17,4 +17,12 @@ describe Event do
     a.end_date = "2011-01-14"
     a.date_range.should == "Monday Jan 3 - Friday Jan 14, 2011"
   end
+
+  it "should give me 'Monday Dec 31, 2011 - Monday Jan 1, 2012' if start year and end year differ" do
+    a = Factory :event
+    a.start_date = "2011-12-30"
+    a.end_date = "2012-01-01"
+    a.date_range.should == "Friday Dec 30, 2011 - Sunday Jan 1, 2012"
+  end
+
 end
