4a53bcb54b67f2e82c66a69c0cd3f894f9621063
Jack Desert <JackDesert556@gmail.com>
2012-01-25 20:00:10 -0500

    placeholder

---
+++ features/step_definitions/custom_web_steps.rb
@@ -0,0 +1,22 @@
+Then /^shoot "([^"]*)"$/ do |filename|
+  # Puts screenshots in /tmp/screenshots
+  # These snippets were lifted out of the capybara-screenshot gem
+  unless filename.empty?
+    file_base_name = filename.strip.downcase.collude
+  else
+    file_base_name = "#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}"
+  end
+  screenshot_path = Rails.root.join "/tmp/screenshots"
+  Dir.mkdir(screenshot_path) unless Dir.exists? screenshot_path
+  file_name = "#{screenshot_path}/#{file_base_name}.png"
+  if Capybara.current_driver == :selenium
+    Capybara.page.driver.browser.save_screenshot(file_name)
+  else
+    puts "To save a screenshot, put '@javascript' before this scenario"
+  end
+end
+
+Given /^I go to the "([^"]*)" event roster page$/ do |event_name|
+  event = Event.find_by_event_name(event_name)
+  visit roster_path(event)
+end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -209,20 +209,3 @@ Then /^show me the page$/ do
   save_and_open_page
 end
 
-Then /^shoot "([^"]*)"$/ do |filename|
-  # Puts screenshots in /tmp/screenshots
-  # These snippets were lifted out of the capybara-screenshot gem
-  unless filename.empty?
-    file_base_name = filename.strip.downcase.collude
-  else
-    file_base_name = "#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}"
-  end
-  screenshot_path = Rails.root.join "/tmp/screenshots"
-  Dir.mkdir(screenshot_path) unless Dir.exists? screenshot_path
-  file_name = "#{screenshot_path}/#{file_base_name}.png"
-  if Capybara.current_driver == :selenium
-    Capybara.page.driver.browser.save_screenshot(file_name)
-  else
-    puts "To save a screenshot, put '@javascript' before this scenario"
-  end
-end
